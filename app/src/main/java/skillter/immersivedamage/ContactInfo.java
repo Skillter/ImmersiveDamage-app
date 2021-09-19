b73851d7c8e8916780c35904a8a907a202ab58be
Skillter <57605954+Skillter@users.noreply.github.com>
2021-09-19 04:24:25 +0200

    Update ContactInfo.java
    Updated links

--- app/src/main/java/skillter/immersivedamage/ContactInfo.java
+++ app/src/main/java/skillter/immersivedamage/ContactInfo.java
@@ -4,7 +4,7 @@ public class ContactInfo {
 
     public static final String DISCORD_USERNAME = "Skillter#3909";
     public static final String GITHUB_ACCOUNT_LINK = "https://github.com/Skillter";
-    public static final String APP_PROJECT_LINK = null;
-    public static final String MOD_PROJECT_LINK = null;
+    public static final String APP_PROJECT_LINK = "https://github.com/Skillter/ImmersiveDamage-app";
+    public static final String MOD_PROJECT_LINK = "https://github.com/Skillter/ImmersiveDamage";
 
 }
